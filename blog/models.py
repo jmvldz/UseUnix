605a3cc2c6998bf22c81814f0dc9fe721124cd17
Josh <exxonvaldeez@gmail.com>
2011-07-16 13:40:25 +0000

    Added completed category and entry models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tagging.fields import TagField
+from markdown import markdown
 import datetime
 
 class Category(models.Model):
@@ -17,8 +20,48 @@ class Category(models.Model):
         return "/categories/%s/" % self.slug
 
 class Entry(models.Model):
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
+    STATUS_CHOICES = (
+        (LIVE_STATUS, 'Live'),
+        (DRAFT_STATUS, 'Draft'),
+        (HIDDEN_STATUS, 'Hidden'),
+    )
+
+    # Core fields
     title = models.CharField(max_length=250)
     excerpt = models.TextField(blank=True)
     body = models.TextField()
     pub_date = models.DateTimeField(default=datetime.datetime.now)
+
+    # Generated HTML fields
+    excerpt_html = models.TextField(editable=False, blank=True)
+    body_html = models.TextField(editable=False, blank=True)
+
+    # Metadata
+    author = models.ForeignKey(User)
+    enable_comments = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
     slug = models.SlugField(unique_for_date='pub_date')
+    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+
+    # Categorization
+    categories = models.ManyToManyField(Category)
+    tags = TagField()
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name_plural = "Entries"
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, force_insert=False, force_update=False):
+        self.body_html = markdown(self.body)
+        if self.excerpt:
+            self.excerpt_html = markdown(self.excerpt)
+        super(Entry, self).save(force_insert, force_update)
+
+    def get_absolute_url(self):
+        return "/weblog/%s/%s/" %s (self.pub_date.strftime("%Y/%b/%d").lower(), self.slug)

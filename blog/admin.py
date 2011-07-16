cc85908c96bded488bad234fb0e6e17ab1f0fb41
Josh <exxonvaldeez@gmail.com>
2011-07-16 11:07:14 +0000

    Building blog module, added categories

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from UseUnix.models import Category
+from blog.models import Category, Entry
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = { 'slug': ['title'] }
+
+class EntryAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug': ['title'] }
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Entry, EntryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+import datetime
 
 class Category(models.Model):
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=250, help_text='Maximum 250 characters.')
+    slug = models.SlugField(unique=True, help_text="Suggested value automatically generated from title. Must be unique.")
     description = models.TextField()
 
     class Meta:
-        verbose_name_pural = "Categories"
+        ordering = ['title']
+        verbose_name_plural = "Categories"
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return "/categories/%s/" % self.slug
+
+class Entry(models.Model):
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(blank=True)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    slug = models.SlugField(unique_for_date='pub_date')
